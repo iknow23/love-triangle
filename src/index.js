eeeb4a0fa24a8b8536b609467047e6f19654f93d
Айнур-ПК\Айнур <felix-xilef@mail.ru>
2019-02-27 10:11:24 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -5,7 +5,18 @@
 module.exports = function getLoveTrianglesCount(preferences = []) {
   var matches = 0;
   for (var i = 0; i < preferences.length; i++) {
-    numA = preferences[i];
-
+    var aLover = preferences[i] - 1;
+    var bLover = preferences[aLover] - 1;
+    var cLover = preferences[bLover] - 1;
+    if (aLover !== i) {
+      if (aLover !== bLover) {
+        if (cLover !== bLover) {
+          if (cLover == i) {
+            matches++;
+          }
+        }
+      }
+    }
   }
+  return matches / 3;
 };
